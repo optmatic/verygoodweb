b69307afc99788b77378667af09c93a9125a0048
thomas <tom@optmatic.com>
2025-01-05 16:44:08 +1000

    before contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 // import PrimaryHero from "@/components/hero-sections/PrimaryHero";
 import CS from "@/components/optmatic/CS";
-
+import Contact from "@/components/optmatic/Contact";
 export default function Home() {
   return (
     <div>
       {/* <PrimaryHero /> */}
       <CS />
+      <Contact />
     </div>
   );
 }
--- src/components/optmatic/CS.tsx
+++ src/components/optmatic/CS.tsx
@@ -1,8 +1,10 @@
 import Logo from "@/images/optmatic-logo.svg";
+// import Contact from "./Contact";
 
 export default function CS() {
     return (
-        <div className="p-[31px] bg-deepBlue/50 border border-zinc-600/5 shadow-md shadow-black/20 flex flex-col justify-between">
+        <section className="flex min-h-screen justify-center items-center">
+        <div className="px-[40px] py-[25px] bg-deepBlue/50 border border-zinc-600 shadow-md shadow-white/5 flex flex-col justify-between">
                 <h1 className="text-3xl text-white sm:text-4xl font-normal font-serif mb-2">We&apos;ll be right back</h1>
                 <p className="text-md text-white font-light">Optmatic is getting an upgrade.</p>
                 <div className="flex flex-col mt-[50px] !text-sm">
@@ -12,5 +14,7 @@ export default function CS() {
                     <img src={Logo.src} alt="Optmatic Logo" className="w-[100px] -mt-[31px] self-end" />
                 </div>
         </div>
+        {/* <Contact /> */}
+        </section>
     )
 }
