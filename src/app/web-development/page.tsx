7379a52050ff2faf81db26040b4a436877cc6846
thomas <tom@optmatic.com>
2025-01-22 09:09:32 +1000

    svg li

--- src/app/web-development/page.tsx
+++ src/app/web-development/page.tsx
@@ -43,16 +43,20 @@ export default function WebDevelopment() {
       buttonLink="/web-development"
     >
       <SimpleContent
-      badge="Channel your eliteness"
-      title="A better workflow"
-      leftColumnContent={`**Lorem Ipsum** is simply dummy text of the printing and typesetting industry. _Lorem Ipsum_ has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, [remaining essentially unchanged](https://www.example.com).
-`}
-      rightColumnContent={`
-Another paragraph with markdown support.
+      badge="Optimise Your Digital Presence"
+      title="A Streamlined Workflow"
+      leftColumnContent={`We simplify the web development process to ensure it's smooth and efficient, saving you time and resources whilst delivering outstanding results.
 
-- List item 1
-- List item 2
-      `}
+**With our services, you'll benefit from:**
+
+- Custom web solutions tailored to your business needs
+- Enhanced website performance for an improved user experience
+- Expert guidance throughout the development journey
+- Regular updates, maintenance, and post-launch support.
+`}
+      rightColumnContent={`Our approach ensures that your website isn't just visually appealing but also fully functional and optimised for performance. We prioritise user experience, ensuring that your visitors have a seamless interaction with your site, which helps boost engagement and conversions.
+
+Ready to elevate your digital presence? Let's collaborate to create a high-performing website that meets your goals and adapts as your business grows.`}
       ctaText="Get started"
       ctaLink="/get-started"
       imageSrc="https://tailwindui.com/plus/img/component-images/project-app-screenshot.png"
